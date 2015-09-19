636a7e62f3259a5a1a8c96dc7f744ac078590da7
magorium <magorium@users.noreply.github.com>
2015-09-19 15:05:09 +0200

    Check-in unit for hostlib.resource

---
+++ Libraries/hostlib/hostlib.pas
@@ -0,0 +1,50 @@
+unit hostlib;
+ 
+
+{
+  Hostlib.resource
+}
+ 
+
+interface
+ 
+
+uses
+  Exec;
+
+
+type
+  PVoid         = pointer;
+
+ 
+const
+  HOSTLIBNAME   = 'hostlib.resource';
+
+
+var
+  HostLibBase   : pLibrary;
+ 
+ 
+  function  HostLib_Open(const filename: PChar; error: PPChar): PVoid; syscall HostLibBase 1;
+  function  HostLib_Close(handle: PVoid; error: PPChar): integer; syscall HostLibBase 2;
+  function  HostLib_GetPointer(handle: PVoid; const symbol: PChar; error: PPChar): PVoid; syscall HostLibBase 3;
+  procedure HostLib_FreeErrorStr(error: PPChar); syscall HostLibBase 4;
+  function  HostLib_GetInterface(handle: PVoid; const symbols: PPChar; unresolved: PULONG): PAPTR; syscall HostLibBase 5;
+  procedure HostLib_DropInterface(interface_: PAPTR); syscall HostLibBase 6;
+  procedure HostLib_Lock; syscall HostLibBase 7;
+  procedure HostLib_Unlock; syscall HostLibBase 8;
+ 
+ 
+implementation
+ 
+ 
+Initialization
+
+  HostLibBase := OpenResource(HOSTLIBNAME);
+ 
+
+finalization
+
+  // resources do not need to be closed
+
+end.
